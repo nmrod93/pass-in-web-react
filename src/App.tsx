553a5febb26b78a5f5e2de87f8ee4bc715d12480
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-04-07 19:29:20 -0300

    feat: adding tailwind dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+interface MeuBotaoProps {
+  texto: string
+}
+
+function MeuBotao(props: MeuBotaoProps) {
+  return <button className="bg-orange-400">{props.texto}</button>
+}
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <MeuBotao texto="Botão 1" />
+      <MeuBotao texto="Botão 2" />
+      <MeuBotao texto="Botão 3" />
+    </div>
+  )
 }
 
