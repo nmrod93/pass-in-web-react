c7a92f4a093c999e96257e6bb076d40eb96d1317
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-04-07 20:12:46 -0300

    feat: creating table

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -2,12 +2,52 @@ import { Search } from "lucide-react"
 
 export function AttendeeList() {
     return (
-        <div className="flex gap-3 items-center">
-            <h1 className="text-2xl font-bold">Participantes</h1>
-            <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
-                <Search className="size-4 text-emerald-300" />
-                <input className="bg-transparent flex-1 outline-none" placeholder="Buscar participante" />
+        <div>
+            <div className="flex gap-3 items-center">
+                <h1 className="text-2xl font-bold">Participantes</h1>
+                <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+                    <Search className="size-4 text-emerald-300" />
+                    <input className="bg-transparent flex-1 outline-none" placeholder="Buscar participante" />
+                </div>
             </div>
-        </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox" />            
+                        </th>
+                        <th>Código</th>
+                        <th>Participação</th>
+                        <th>Data de inscrição</th>
+                        <th>Data de check-in</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="checkbox" />                
+                        </td>
+                        <td>12383</td>
+                        <td>
+                            <div>
+                                <span>Nicolas Montoro Rodrigues</span>
+                                <span>nicolas@gmail.com</span>
+                            </div>
+                        </td>
+                        <td>7 dias atrás</td>
+                        <td>3 dias atrás</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={3}>Mostrando 10 de 228 items</td>
+                        <td colSpan={3}>Página 1 de 23</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>        
     )
 }
